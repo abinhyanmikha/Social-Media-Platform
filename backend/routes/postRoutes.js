d682cc6d84a43a3ebff8bee90393d5bdfa1e50bf
abinsuwal <abinsuwal650@gmail.com>
2026-01-16 21:34:28 +0545

    backend like

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -21,6 +21,7 @@ router.post("/", auth, async (req, res) => {
 });
 
 /**
+ * 
  * GET ALL POSTS (Feed)
  */
 router.get("/", auth, async (req, res) => {
@@ -34,5 +35,35 @@ router.get("/", auth, async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+/**
+ * LIKE / UNLIKE POST
+ */
+router.put("/:id/like", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const userId = req.user._id.toString();
+
+    if (post.likes.includes(userId)) {
+      // UNLIKE
+      post.likes = post.likes.filter(
+        (id) => id.toString() !== userId
+      );
+    } else {
+      // LIKE
+      post.likes.push(userId);
+    }
+
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 module.exports = router;
