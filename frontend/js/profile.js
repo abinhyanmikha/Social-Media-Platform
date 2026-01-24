1ae8b6a22e688578ce3a93437e61860932f97cd0
abinsuwal <abinsuwal650@gmail.com>
2026-01-24 21:05:12 +0545

    add view followers and following

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,72 +1,107 @@
-const token = localStorage.getItem("token");
-if (!token) window.location.href = "index.html";
-
-const params = new URLSearchParams(window.location.search);
-const userId = params.get("id");
-
-const USER_API = "http://localhost:5000/api/users";
+const API_URL = "http://localhost:5000/api/users";
 const POST_API = "http://localhost:5000/api/posts";
 
-const myId = JSON.parse(atob(token.split(".")[1])).id;
+const token = localStorage.getItem("token");
+
+function getProfileId() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("id");
+}
+
+function getUserIdFromToken() {
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  return payload.id;
+}
+
+function openModal(title, list) {
+  document.getElementById("modalTitle").innerText = title;
+
+  const modalList = document.getElementById("modalList");
+  modalList.innerHTML = "";
+
+  list.forEach((user) => {
+    modalList.innerHTML += `
+      <p>
+        <a href="profile.html?id=${user._id}">
+          ${user.username}
+        </a>
+      </p>
+    `;
+  });
+
+  document.getElementById("listModal").style.display = "flex";
+}
+
+document.getElementById("closeModal").onclick = () => {
+  document.getElementById("listModal").style.display = "none";
+};
+
+async function loadPosts(userId) {
+  const res = await fetch(`${POST_API}/user/${userId}`, {
+    headers: { Authorization: "Bearer " + token },
+  });
+  const posts = await res.json();
+
+  document.getElementById("postCount").innerText = posts.length;
+
+  document.getElementById("posts").innerHTML = posts
+    .map(
+      (p) => `
+      <div class="post">
+        <p><strong>${p.user.username}</strong></p>
+        <p>${p.content}</p>
+      </div>
+    `,
+    )
+    .join("");
+}
 
 async function loadProfile() {
   const userId = getProfileId();
-  const res = await fetch(`${USER_API}/${userId}`, {
+
+  const res = await fetch(`${API_URL}/${userId}`, {
     headers: { Authorization: "Bearer " + token },
   });
+
   const user = await res.json();
 
   document.getElementById("username").innerText = user.username;
   document.getElementById("followers").innerText = user.followers.length;
   document.getElementById("following").innerText = user.following.length;
 
+  document.getElementById("followersBtn").onclick = () => {
+    openModal("Followers", user.followers);
+  };
+
+  document.getElementById("followingBtn").onclick = () => {
+    openModal("Following", user.following);
+  };
+
+  // follow/unfollow button
+  const currentUserId = getUserIdFromToken();
   const followBtn = document.getElementById("followBtn");
 
-  if (user._id === myId) {
+  if (currentUserId === user._id.toString()) {
     followBtn.style.display = "none";
   } else {
-    followBtn.innerText = user.followers.includes(myId) ? "Unfollow" : "Follow";
-    followBtn.onclick = toggleFollow;
+    followBtn.innerText = user.followers.some(
+      (u) => u._id.toString() === currentUserId,
+    )
+      ? "Unfollow"
+      : "Follow";
+
+    followBtn.onclick = async () => {
+      await fetch(`${API_URL}/${userId}/follow`, {
+        method: "PUT",
+        headers: { Authorization: "Bearer " + token },
+      });
+
+      loadProfile(); // refresh
+    };
   }
 
-  loadPosts();
+  // load profile posts
+  loadPosts(userId);
 }
 
-async function toggleFollow() {
-  await fetch(`${USER_API}/${userId}/follow`, {
-    method: "PUT",
-    headers: { Authorization: "Bearer " + token },
-  });
-  loadProfile();
-}
-
-async function loadPosts() {
-  const res = await fetch(`${POST_API}/user/${userId}`, {
-    headers: { Authorization: "Bearer " + token },
-  });
-  const posts = await res.json();
-
-  const postsDiv = document.getElementById("posts");
-  postsDiv.innerHTML = "";
-
-  document.getElementById("postCount").innerText = posts.length;
-
-  posts.forEach((post) => {
-    const div = document.createElement("div");
-    div.className = "profile-post";
-    div.innerText = post.content;
-    postsDiv.appendChild(div);
-  });
-}
-function logout() {
-  localStorage.removeItem("token");
-  window.location.href = "index.html";
-}
-function getProfileId() {
-  const params = new URLSearchParams(window.location.search);
-  return params.get("id");
-}
-
-window.onload = () => {
-  loadProfile();
-};
+loadProfile();
